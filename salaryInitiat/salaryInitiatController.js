cd2e3d91776a83984dbc449c5699f1d26edc3273
alokekantal <a25.kantal@gmail.com>
2018-07-29 01:05:15 +0530

    model binding in salary initiation

--- salaryInitiat/salaryInitiatController.js
+++ salaryInitiat/salaryInitiatController.js
@@ -1,10 +1,20 @@
 (function () {
     'use strict';
 	angular.module('employeeApp', ['nvd3', 'moment-picker'])
-    .controller('salaryInitiatController', salaryInitiatController)
-    function salaryInitiatController() {
+    .controller('salaryInitiatController', ['$q', 'salaryService', function($q, salaryService) {
         var vm = this; 
         vm.title = 'Salary Initiation';
+        vm.currentYearMonth = moment();
+
+        vm.init = function(){ 
+            var promiseArray = [];
+            promiseArray.push(salaryService.getEmpoloyeeSalaryDetail());
+            $q.all(promiseArray).then(function(res){
+                vm.employeesSalary = res[0];
+            }, function(err){
+                console.log(err);
+            });           
+        }
  
-    }
+    }]);
 })();
---
+++ salaryInitiat/salaryInitiatService.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+	angular.module('employeeApp')
+    .service('salaryService', ['$q', '$http', function($q, $http){
+        var getEmpoloyeeSalaryDetail = function () {
+            var deferred = $q.defer();
+            $http({
+                url: '../master/EmployeeSalary.json',
+                method: 'GET'
+            }).then(function(res){
+                deferred.resolve(res.data);
+            }, function(res){
+                return deferred.reject(res);
+            });
+
+            return deferred.promise;
+        }
+        return {
+            getEmpoloyeeSalaryDetail: getEmpoloyeeSalaryDetail
+        }
+    }])
+})();
